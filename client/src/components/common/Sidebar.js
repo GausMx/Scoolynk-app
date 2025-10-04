72c51408b92263429b0f3fb035dfcc3ae7b90921
Gaus Lawal <lawalgaus7@gmail.com>
2025-10-04 07:40:25 +0100

    new styles

--- client/src/components/common/Sidebar.js
+++ client/src/components/common/Sidebar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import './Sidebar.css';
 
 const Sidebar = ({ user, role }) => {
   const [isOpen, setIsOpen] = useState(true);
