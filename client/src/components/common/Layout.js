ef2f1a8efd79033e34bc49dddc19f59cc75d8a40
Gaus Lawal <lawalgaus7@gmail.com>
2025-10-04 08:21:34 +0100

    new

--- client/src/components/common/Layout.js
+++ client/src/components/common/Layout.js
@@ -1,10 +1,10 @@
 // src/components/common/Layout.js
 import React from 'react';
 import Sidebar from './Sidebar';
-
+import './Layout.css';
 const Layout = ({ user, role, children }) => {
   return (
-    <div className="d-flex">
+    <div className="main-content d-flex">
       <Sidebar user={user} role={role} />
       <main className="flex-grow-1 p-4">
         {children}
