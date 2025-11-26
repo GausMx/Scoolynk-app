ff1239c1ce074d099ca8c151565f9ab3f29f82fd
Gaus Lawal <lawalgaus7@gmail.com>
2025-11-26 03:42:09 +0100

    new

--- server/server.js
+++ server/server.js
@@ -77,6 +77,19 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 // -----------------------------------------------------
+// 🚫 PREVENT API CACHING (CRITICAL FOR PWA)
+// -----------------------------------------------------
+app.use('/api', (req, res, next) => {
+  res.set({
+    'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+    'Pragma': 'no-cache',
+    'Expires': '0',
+    'Surrogate-Control': 'no-store'
+  });
+  next();
+});
+
+// -----------------------------------------------------
 // 🔐 SECURITY MIDDLEWARE
 // -----------------------------------------------------
 
